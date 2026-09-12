96e3c948b62f7293feeb34cc319c428697100cf7
dy00214-eng <dy00214@gmail.com>
2026-09-11 21:19:28 -1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "meal-pa-v1";
+const FILES = [
+  "./",
+  "./meal-pa.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.all(FILES.map(f => c.add(f).catch(() => {}))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 캐시 우선: 비행기 모드에서도 저장된 화면을 그대로 연다
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if(req.method !== "GET") return;
+  if(new URL(req.url).origin !== self.location.origin) return;  // API 호출은 통과
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./meal-pa.html")))
+  );
+});
